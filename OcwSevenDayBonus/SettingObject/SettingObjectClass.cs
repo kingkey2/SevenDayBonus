2c4bcddbccd4afe436346502271c0fa9a758a27f
kingkey2 <132646006+kingkey2@users.noreply.github.com>
2023-05-05 17:44:01 +0800

    Add files via upload

---
+++ OcwSevenDayBonus/SettingObject/SettingObjectClass.cs
@@ -0,0 +1,5 @@
+﻿public class SettingObjectClass {
+    public bool IsTest = true;
+    public string ServerAPI = "http://ocw.dev.mts.idv.tw/API/MgmtAPI.asmx";
+    public string SetSevenDateBonusAPI = "http://ocw.dev.mts.idv.tw/API/MgmtAPI.asmx/SetSevenDateBonusForConsole";
+}
